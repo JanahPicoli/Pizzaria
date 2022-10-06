234fef613c24240b2c616bf8352c530083f11032
Janayna C N de Picoli <109486957+JanahPicoli@users.noreply.github.com>
2022-10-06 18:49:21 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,145 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        price: 25.00,
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: 26.00,
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: 28.00,
+        description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+      },
+      {
+        id: 4,
+        name: 'Brasileira',
+        img: 'images/pizza4.png',
+        price: 30.00,
+        description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/pizza5.png',
+        price: 29.00,
+        description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+      },
+      {
+        id: 7,
+        name: 'Banana com canela',
+        img: 'images/pizza7.png',
+        price: 32.00,
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'images/pizza8.png',
+        price: 35.00,
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      },
+      {
+        id: 9,
+        name: 'Pimenta',
+        img: 'images/pizza9.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, pimenta caiena, pimenta malagueta'
+      },
+      {
+        id: 10,
+        name: 'Peperoni',
+        img: 'images/pizza10.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, peperoni e orégano'
+      },
+      {
+        id: 11,
+        name: 'Frango com catupiry',
+        img: 'images/pizza11.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, frango desfiado, catupiry e orégano'
+      },
+      {
+        id: 12,
+        name: 'Lombinho com caturpiry',
+        img: 'images/pizza12.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, lombinho, catupiry e orégano'
+      },
+      {
+        id: 13,
+        name: 'Rúcula com tomate seco',
+        img: 'images/pizza13.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, rúcula, tomates secos, lascas de castanhas e orégano'
+      },
+      {
+        id: 14,
+        name: 'Bacon',
+        img: 'images/pizza14.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, bacon e orégano'
+      },
+      {
+        id: 15,
+        name: 'Presunto',
+        img: 'images/pizza15.png',
+        price: 35.00,
+        description: 'Molho de tomate, mussarela, presunto, tomates, azeitona, e orégano'
+      },
+      {
+        id: 16,
+        name: 'Água',
+        img: 'images/agua.png',
+        price: 5.00,
+        description: 'Agua mineral sem gás'
+      },
+      {
+        id: 17,
+        name: 'Sucos',
+        img: 'images/suco.png',
+        price: 10.00,
+        description: 'Suco de frutas'
+      },
+      {
+        id: 18,
+        name: 'Refrigerante',
+        img: 'images/coca.png',
+        price: 15.00,
+        description: 'Refrigerante de cola'
+      },
+      {
+        id: 19,
+        name: 'Cerveja',
+        img: 'images/cerva.png',
+        price: 15.00,
+        description: 'Cerveja puro malte'
+      },
+      {
+        id: 20,
+        name: 'Vinho',
+        img: 'images/vinh.png',
+        price: 15.00,
+        description: 'Vinho tinto seco ou Vinho branco'
+      },
+      
+      ];
+
+
